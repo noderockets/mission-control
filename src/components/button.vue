2ae3662c02bac7e82e408dd85094abec026dab69
Justin Permann <justinpermann@gmail.com>
2018-08-11 15:29:50 -0600

    created a new button component

---
+++ src/components/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="button" @click="onClick">{{label}}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    label: String
+  },
+  methods: {
+    onClick() {
+      this.$emit("click")
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  color: #fff;
+  background-color: #6ebd4f;
+  border-radius: 8px;
+  border: 1px solid #c0c0c0;
+  font-size: 16px;
+  height: 50px;
+}
+</style>
